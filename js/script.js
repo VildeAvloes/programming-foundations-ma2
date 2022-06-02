ee923d8ed636a2fc4b189d51d8da7d83fb2e0d25
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2022-06-02 08:05:48 +0200

    First question done

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Question 1
+
+var pets = [
+  {
+    type: "cat",
+    age: 5.5,
+  },
+  {
+    type: "dog",
+    age: 3.8,
+  },
+  {
+    type: "parrot",
+    age: 4.0,
+  },
+];
+
+for (var i = 0; i < pets.length; i++) {
+  if (pets[i].age >= 4) {
+    console.log(pets[i].type);
+  }
+}
+
+//Question 2
+
+//Question 3
