716ae4a102a31542cd87738a526cf2ca6ed2cf64
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2022-06-10 14:59:53 +0200

    Question 2 and 3

--- js/script.js
+++ js/script.js
@@ -22,5 +22,21 @@ for (var i = 0; i < pets.length; i++) {
 }
 
 //Question 2
+function checkBooleanValue(plant) {
+  if (typeof plant === "boolean") {
+    console.log("checkBooleanValue");
+  } else console.log("Please pass a boolean value.");
+}
+
+checkBooleanValue("Monstera");
 
 //Question 3
+var subheading = document.querySelector("h2");
+var button = document.querySelector("button");
+
+function changeSubheading() {
+  subheading.innerHTML = "Updated subheading";
+  subheading.style.color = "blue";
+}
+
+button.onclick = changeSubheading;
